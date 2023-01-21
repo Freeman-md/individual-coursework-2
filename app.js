ede1caca0719cfb2153e6ec8992c918154c8e4ce
Freeman-md <66797587+Freeman-md@users.noreply.github.com>
2023-01-21 17:52:54 +0000

    UPDATE: endpoint to create order

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 const { connectToDb, getDb } = require("./db");
 const logger = require("./logger");
 
@@ -7,6 +8,7 @@ const app = express();
 
 app.use(express.static("public"));
 app.use(logger)
+app.use(bodyParser.json());
 app.use(cors());
 
 app.use((err, req, res, next) => {
@@ -27,9 +29,28 @@ connectToDb()
 app.get("/lessons", async (req, res, next) => {
   try {
     const db = getDb();
-    const collection = db.collection("lessons");
+    const collection = db.collection("lesson");
     const items = await collection.find({}).toArray();
     res.send(items);
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.post("/orders", async (req, res, next) => {
+  try {
+    const order = req.body
+
+    console.log(order)
+
+    const db = getDb();
+    const collection = db.collection("orders");
+    
+    collection.insertOne(order, (err, result) => {
+      if (err) throw err
+
+      res.json(result)
+    })
   } catch (err) {
     next(err);
   }
