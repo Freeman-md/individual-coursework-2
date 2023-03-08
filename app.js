0c4c51f11b6bf6b48fc883641b6ba7e18194bdb7
Freeman-md <66797587+Freeman-md@users.noreply.github.com>
2023-03-08 09:13:30 +0000

    UPDATE: update cors implementation

--- app.js
+++ app.js
@@ -12,12 +12,6 @@ app.use(express.static("public"));
 app.use(logger);
 app.use(bodyParser.json());
 app.use(cors())
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 
 app.use((err, req, res, next) => {
   console.log("Error: ", err);
